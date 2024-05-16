43f849ba5d4396ef0461039136d20d1352078430
sbk <Stanimir.Kostadinov@inov8consulting.co.uk>
2024-05-16 07:53:49 +0100

    Added ReactEssesntials15-PlacePickerProject-HTTPRequestsuseEffect porject

---
+++ ReactEssesntials15-PlacePickerProject-HTTPRequestsuseEffect/places-picker-fullstack/01-starting-project/src/components/AvailablePlaces.jsx
@@ -0,0 +1,30 @@
+
+import { useEffect, useState } from 'react';
+import Places from './Places.jsx';
+
+const places = localStorage.getItem('places');
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  // TOODO: Fetch available places from backend API
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect ( ()=> {
+    fetch('https://localhost:3000/places')
+      .then( (response) => {
+        return response.json();
+      })
+      .then( (resData) => {
+        setAvailablePlaces(resData.places);
+      });
+    
+  }, []);
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
