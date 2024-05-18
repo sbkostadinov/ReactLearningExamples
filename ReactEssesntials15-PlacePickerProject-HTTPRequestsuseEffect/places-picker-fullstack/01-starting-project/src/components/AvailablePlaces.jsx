6429d96e41671d1a08685893038edea6c6ab5a8e
sbk <Stanimir.Kostadinov@inov8consulting.co.uk>
2024-05-18 09:12:19 +0100

    Added navigator.geolocation.getCurrentPosition to Availableplaces.jsx to use current position for sorting Places by Distance

--- ReactEssesntials15-PlacePickerProject-HTTPRequestsuseEffect/places-picker-fullstack/01-starting-project/src/components/AvailablePlaces.jsx
+++ ReactEssesntials15-PlacePickerProject-HTTPRequestsuseEffect/places-picker-fullstack/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,5 +1,7 @@
 
 import { useEffect, useState } from 'react';
+import { sortPlacesByDistance } from '../loc.js';
+import { Error } from './Error.jsx';
 import Places from './Places.jsx';
 
 //const places = localStorage.getItem('places');
@@ -7,20 +9,42 @@ import Places from './Places.jsx';
 export default function AvailablePlaces({ onSelectPlace }) {
   // TOODO: Fetch available places from backend API
   const [availablePlaces, setAvailablePlaces] = useState([]);
-
+  const [isFetching, setIsFetching ] = useState(false);
+  const [error, setError] = useState(false);
   
 
   useEffect ( ()=> { 
-
     async function  fetchPlacesData () {
-        const response = await fetch('http://localhost:3000/places');
-        const resData = await response.json();
-        setAvailablePlaces(resData.places);
+        setIsFetching(true);
+        try {
+          const response = await fetch('http://localhost:3000/places');
+          const resData = await response.json();
+          if (!response.ok) {
+              throw new Error('Failed to fetch places');
+          }
+
+          navigator.geolocation.getCurrentPosition( (position) => {
+            const sortedPlaces = sortPlacesByDistance(
+              resData.places, position.coords.latitude, position.coords.longitude ); 
+
+            setAvailablePlaces(sortedPlaces); 
+            setIsFetching(false);
+          });
+
+         }
+         catch (error) {
+          setError({message: error.message || "Cloud not fetch data for places. Please try again later!"});
+          setIsFetching(false);
+        }
     }
 
     fetchPlacesData();
       
   }, []);
+
+  if (error) {
+    return  <Error title="An Error took place" message={error.message}/>;
+  }
 
 
 
@@ -28,6 +52,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
